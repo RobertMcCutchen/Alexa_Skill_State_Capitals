23a39d7b66a3792720d17010cbcc0a346c9efff8
RobertMcCutchen <rmccutchen2009@gmail.com>
2020-11-15 17:07:50 -0600

    Create Ask_Capital.py

---
+++ Ask_Capital.py
@@ -0,0 +1,55 @@
+import boto3
+ddb = boto3.client("dynamodb")
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        handler_input.response_builder.speak("Welcome to the state capitals skill!").set_should_end_session(False)
+        return handler_input.response_builder.response    
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    def can_handle(self, handler_input, exception):
+        return True
+
+    def handle(self, handler_input, exception):
+        print(exception)
+        handler_input.response_builder.speak("Sorry, would you mind repeating that?")
+        return handler_input.response_builder.response
+
+class StateCapitalIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("Ask_Capital")(handler_input)
+
+    def handle(self, handler_input):
+        state = handler_input.request_envelope.request.intent.slots['state'].value
+
+        '''try:
+            data = ddb.get_item(
+                TableName="ChineseAnimal",
+                Key={
+                    'BirthYear': {
+                        'N': year
+                    }
+                }
+            )
+        except BaseException as e:
+            print(e)
+            raise(e)
+        '''
+        speech_text = "The capital of {} is unknown".format(state)
+        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+        return handler_input.response_builder.response    
+
+sb = SkillBuilder()
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_exception_handler(CatchAllExceptionHandler())
+sb.add_request_handler(StateCapitalIntentHandler())
+
+def handler(event, context):
+    return sb.lambda_handler()(event, context)
